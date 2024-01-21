e604d9c0829fed6c1d705b9ba7cd0a80f7e126b3
Hamza <jhamzaahmed@gmail.com>
2024-01-21 11:52:29 +0500

    event emitter and listener

--- events_module.js
+++ events_module.js
@@ -1,1 +1,28 @@
- 
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+// register a listener
+// listener must be registered above the registered event
+// if listener is passed additional data, it appears in arguments of the callback
+// emitter.on("messageLogged", (arg) => {
+//   console.log("Listener Called ", arg);
+// });
+
+// raised an event
+// this event will be raised when a listener is registered
+// emitter.emit("messageLogged");
+
+// an event can have additional optional arguments
+// its better to encapsulate those arguments in an object but not necessary
+// these arguments carry data over events
+// emitter.emit("messageLogged", { id: 1, url: "http://" });
+
+const Logger = require("./logger");
+const logger = new Logger();
+
+logger.on("messageLogged", (arg) => {
+  console.log("Listener Called ", arg);
+});
+
+logger.log("message");
